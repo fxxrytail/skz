5c0750655d133fa9b9d824ce792ed36cb537e543
fxxrytail <42585714+fxxrytail@users.noreply.github.com>
2018-11-02 01:43:48 +0800

    Delete h.php

--- h.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html>
-<body>
-<div align=center><img src='https://www.counter12.com/img-dwCa9460A2a9DzWx-57.gif' border='0' alt='contador de visitas'><script type='text/javascript' src='https://www.counter12.com/ad.js?id=dwCa9460A2a9DzWx'></script></div>
-
-<?php echo "get cool~" ?>
-</body>
-</html>
